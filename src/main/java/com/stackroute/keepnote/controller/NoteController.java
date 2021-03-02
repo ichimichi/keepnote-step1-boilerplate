94281e5c6d42ca15006ea0d12a84b9be930f93d5
Laribok.Syiemlieh <laribok.syiemlieh@ibm.com>
2021-03-02 15:29:34 +0530

    implemented note controllers

--- src/main/java/com/stackroute/keepnote/controller/NoteController.java
+++ src/main/java/com/stackroute/keepnote/controller/NoteController.java
@@ -1,11 +1,20 @@
 package com.stackroute.keepnote.controller;
 
+import java.time.LocalDateTime;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.stackroute.keepnote.model.Note;
+import com.stackroute.keepnote.repository.NoteRepository;
 
 /*Annotate the class with @Controller annotation. @Controller annotation is used to mark 
  * any POJO class as a controller so that Spring can recognize this class as a Controller
@@ -13,45 +22,69 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class NoteController {
 	/*
-	 * From the problem statement, we can understand that the application
-	 * requires us to implement the following functionalities.
+	 * From the problem statement, we can understand that the application requires
+	 * us to implement the following functionalities.
 	 * 
-	 * 1. display the list of existing notes from the collection. Each note 
-	 *    should contain Note Id, title, content, status and created date.
-	 * 2. Add a new note which should contain the note id, title, content and status.
-	 * 3. Delete an existing note.
-	 * 4. Update an existing note.
+	 * 1. display the list of existing notes from the collection. Each note should
+	 * contain Note Id, title, content, status and created date. 2. Add a new note
+	 * which should contain the note id, title, content and status. 3. Delete an
+	 * existing note. 4. Update an existing note.
 	 */
-	
-	/* 
-	 * Get the application context from resources/beans.xml file using ClassPathXmlApplicationContext() class.
-	 * Retrieve the Note object from the context.
-	 * Retrieve the NoteRepository object from the context.
+
+	/*
+	 * Get the application context from resources/beans.xml file using
+	 * ClassPathXmlApplicationContext() class. Retrieve the Note object from the
+	 * context. Retrieve the NoteRepository object from the context.
 	 */
-	
-	
-	/*Define a handler method to read the existing notes by calling the getAllNotes() method 
-	 * of the NoteRepository class and add it to the ModelMap which is an implementation of Map 
-	 * for use when building model data for use with views. it should map to the default URL i.e. "/" */
+	ApplicationContext act = new ClassPathXmlApplicationContext("beans.xml");
+	NoteRepository noteRepository = (NoteRepository) act.getBean("noteRepository");
+
+	/*
+	 * Define a handler method to read the existing notes by calling the
+	 * getAllNotes() method of the NoteRepository class and add it to the ModelMap
+	 * which is an implementation of Map for use when building model data for use
+	 * with views. it should map to the default URL i.e. "/"
+	 */
 	@RequestMapping(value = "/")
-	public String getAllNotes(HttpServletRequest req, HttpServletResponse res) {
+	public String getAllNotes(Model model) {
+		model.addAttribute("noteList", noteRepository.getAllNotes());
 		return "index";
 	}
-	
-	
-	/*Define a handler method which will read the Note data from request parameters and
-	 * save the note by calling the addNote() method of NoteRepository class. Please note 
-	 * that the createdAt field should always be auto populated with system time and should not be accepted 
-	 * from the user. Also, after saving the note, it should show the same along with existing 
-	 * notes. Hence, reading notes has to be done here again and the retrieved notes object 
-	 * should be sent back to the view using ModelMap.
-	 * This handler method should map to the URL "/saveNote". 
-	*/
-	
-	
-	/* Define a handler method to delete an existing note by calling the deleteNote() method 
-	 * of the NoteRepository class
-	 * This handler method should map to the URL "/deleteNote" 
-	*/
-	
+
+	/*
+	 * Define a handler method which will read the Note data from request parameters
+	 * and save the note by calling the addNote() method of NoteRepository class.
+	 * Please note that the createdAt field should always be auto populated with
+	 * system time and should not be accepted from the user. Also, after saving the
+	 * note, it should show the same along with existing notes. Hence, reading notes
+	 * has to be done here again and the retrieved notes object should be sent back
+	 * to the view using ModelMap. This handler method should map to the URL
+	 * "/saveNote".
+	 */
+	@RequestMapping(value = "/saveNote", method = RequestMethod.POST)
+	public String saveNote(@RequestParam("noteId") String noteId, @RequestParam("noteTitle") String noteTitle,
+			@RequestParam("noteContent") String noteContent, @RequestParam("noteStatus") String noteStatus) {
+
+		Note note = (Note) act.getBean("note");
+		note.setNoteId(Integer.parseInt(noteId));
+		note.setNoteTitle(noteTitle);
+		note.setNoteContent(noteContent);
+		note.setNoteStatus(noteStatus);
+		note.setCreatedAt(LocalDateTime.now());
+		noteRepository.addNote(note);
+
+		return "index";
+	}
+
+	/*
+	 * Define a handler method to delete an existing note by calling the
+	 * deleteNote() method of the NoteRepository class This handler method should
+	 * map to the URL "/deleteNote"
+	 */
+	@RequestMapping(value = "/deleteNote")
+	public String deleteNote(@RequestParam int noteId) {
+		noteRepository.deleteNote(noteId);
+		return "redirect:" + "/";
+	}
+
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,6 +1,7 @@
 
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8" isELIgnored="false"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -9,10 +10,45 @@
 <title>KeepNote</title>
 </head>
 <body>
-	<h2>Welcome to Keep Notes</h2>
+	<h3>Welcome to Keep Notes</h3>
 	<!-- Create a form which will have text boxes for Note ID, title, content and status along with a Send 
 		 button. Handle errors like empty fields -->
+	<form action="saveNote" method="post">
+		<input placeholder="id" name="noteId" /><br> <input
+			placeholder="title" name="noteTitle" /><br> <input
+			placeholder="content" name="noteContent" /><br> <input
+			placeholder="status" name="noteStatus" /><br> <input type="submit"
+			value="Send" />
+	</form>
 
 	<!-- display all existing notes in a tabular structure with Id, Title,Content,Status, Created Date and Action -->
+
+	<br />
+	<h3>List of Notes</h3>
+	<table border="1">
+		<tr>
+			<th>Id</th>
+			<th>Title</th>
+			<th>Content</th>
+			<th>Status</th>
+			<th>createdAt</th>
+			<th></th>
+		</tr>
+		<c:forEach var="note" items="${noteList}">
+			<tr>
+				<td>${note.noteId}</td>
+				<td>${note.noteTitle}</td>
+				<td>${note.noteContent}</td>
+				<td>${note.noteStatus}</td>
+				<td>${note.createdAt}</td>
+				<td><form action="deleteNote" method="post">
+						<input type="hidden" id="noteId" name="noteId"
+							value="${note.noteId}" />
+						<button type="submit">Delete</button>
+					</form></td>
+			</tr>
+			<br />
+		</c:forEach>
+	</table>
 </body>
 </html>
